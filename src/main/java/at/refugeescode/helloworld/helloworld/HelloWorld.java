c9a35cdc32c9054b5e57859a488cc4275ebc6256
Hadi <hadi.effatian@gmail.com>
2018-03-23 17:49:15 +0100

    Add final version

---
+++ src/main/java/at/refugeescode/helloworld/helloworld/HelloWorld.java
@@ -0,0 +1,11 @@
+package at.refugeescode.helloworld.helloworld;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class HelloWorld {
+
+    public String sayHelloWorld() {
+        return "Hello World";
+    }
+}
